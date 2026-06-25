d4730b1fa54568214609fd815542c87ef35178dd
Kaan <kaanrkaraman@gmail.com>
2026-06-25 16:32:52 +0300

    feat: flesh out FlightData Actions, Servo/Relay, Payload Control and Scripts tabs

--- src/MissionPlannerAvalonia/ViewModels/FlightDataViewModel.cs
+++ src/MissionPlannerAvalonia/ViewModels/FlightDataViewModel.cs
@@ -199,6 +199,205 @@ public partial class FlightDataViewModel : ViewModelBase {
     SelectedMode = "RTL";
     return SetMode();
   }
+
+  // ---- Actions tab ----
+  public ObservableCollection<string> Actions { get; } =
+      new()
+      {
+            "Loiter_Unlim",
+            "Return_To_Launch",
+            "Preflight_Calibration",
+            "Mission_Start",
+            "Preflight_Reboot_Shutdown",
+            "Trigger_Camera",
+            "Battery_Reset",
+            "Toggle_Safety_Switch",
+            "Do_Parachute",
+            "Engine_Start",
+            "Engine_Stop",
+            "Terminate_Flight",
+      };
+
+  [ObservableProperty]
+  private string _selectedAction = "Return_To_Launch";
+
+  [ObservableProperty]
+  private int _setWpIndex;
+
+  [ObservableProperty]
+  private double _homeAlt;
+
+  [RelayCommand]
+  [Obsolete]
+  private async Task DoAction() {
+    if (!Connected) {
+      Messages += "Not connected.\n";
+      return;
+    }
+    var a = SelectedAction;
+    await Task.Run(() => RunAction(a));
+    Log($"Action {a} sent");
+  }
+
+  [Obsolete]
+  private void RunAction(string a) {
+    byte s = _comPort.MAV.sysid;
+    byte c = _comPort.MAV.compid;
+    switch (a) {
+      case "Loiter_Unlim":
+        _comPort.setMode("Loiter");
+        break;
+      case "Return_To_Launch":
+        _comPort.setMode("RTL");
+        break;
+      case "Preflight_Calibration":
+        _comPort.doCommand(s, c, MAVLink.MAV_CMD.PREFLIGHT_CALIBRATION, 1, 1, 0, 0, 0, 0, 0);
+        break;
+      case "Mission_Start":
+        _comPort.doCommand(s, c, MAVLink.MAV_CMD.MISSION_START, 0, 0, 0, 0, 0, 0, 0);
+        break;
+      case "Preflight_Reboot_Shutdown":
+        _comPort.doCommand(s, c, MAVLink.MAV_CMD.PREFLIGHT_REBOOT_SHUTDOWN, 1, 0, 0, 0, 0, 0, 0);
+        break;
+      case "Trigger_Camera":
+        _comPort.doCommand(s, c, MAVLink.MAV_CMD.DO_DIGICAM_CONTROL, 0, 0, 0, 0, 1, 0, 0);
+        break;
+      case "Battery_Reset":
+        _comPort.doCommand(s, c, MAVLink.MAV_CMD.BATTERY_RESET, 255, 100, 0, 0, 0, 0, 0);
+        break;
+      case "Toggle_Safety_Switch":
+        _comPort.doCommand(s, c, MAVLink.MAV_CMD.DO_SET_SAFETY_SWITCH_STATE, 0, 0, 0, 0, 0, 0, 0);
+        break;
+      case "Do_Parachute":
+        _comPort.doCommand(s, c, MAVLink.MAV_CMD.DO_PARACHUTE, 2, 0, 0, 0, 0, 0, 0);
+        break;
+      case "Engine_Start":
+        _comPort.doCommand(s, c, MAVLink.MAV_CMD.DO_ENGINE_CONTROL, 1, 0, 0, 0, 0, 0, 0);
+        break;
+      case "Engine_Stop":
+        _comPort.doCommand(s, c, MAVLink.MAV_CMD.DO_ENGINE_CONTROL, 0, 0, 0, 0, 0, 0, 0);
+        break;
+      case "Terminate_Flight":
+        _comPort.doCommand(s, c, MAVLink.MAV_CMD.DO_FLIGHTTERMINATION, 1, 0, 0, 0, 0, 0, 0);
+        break;
+    }
+  }
+
+  [RelayCommand]
+  private async Task SetWp() {
+    if (!Connected) {
+      Messages += "Not connected.\n";
+      return;
+    }
+    ushort idx = (ushort)SetWpIndex;
+    await Task.Run(() => _comPort.setWPCurrent(_comPort.MAV.sysid, _comPort.MAV.compid, idx));
+    Log($"Set current WP {idx}");
+  }
+
+  [RelayCommand]
+  [Obsolete]
+  private async Task SetHome() {
+    if (!Connected) {
+      Messages += "Not connected.\n";
+      return;
+    }
+    await Task.Run(() =>
+        _comPort.doCommand(_comPort.MAV.sysid, _comPort.MAV.compid, MAVLink.MAV_CMD.DO_SET_HOME,
+            0, 0, 0, 0, 0, 0, (float)HomeAlt));
+    Log($"Set home alt {HomeAlt}");
+  }
+
+  [RelayCommand]
+  [Obsolete]
+  private async Task AbortLand() {
+    if (!Connected) {
+      Messages += "Not connected.\n";
+      return;
+    }
+    await Task.Run(() =>
+        _comPort.doCommand(_comPort.MAV.sysid, _comPort.MAV.compid, MAVLink.MAV_CMD.DO_GO_AROUND,
+            0, 0, 0, 0, 0, 0, 0));
+    Log("Abort landing (go-around) sent");
+  }
+
+  // ---- Servo / Relay tab ----
+  [ObservableProperty]
+  private int _servoNumber = 1;
+
+  [ObservableProperty]
+  private int _servoPwm = 1500;
+
+  private readonly bool[] _relayState = new bool[6];
+
+  [RelayCommand]
+  [Obsolete]
+  private async Task SetServo() {
+    if (!Connected) {
+      Messages += "Not connected.\n";
+      return;
+    }
+    int n = ServoNumber;
+    int pwm = ServoPwm;
+    await Task.Run(() =>
+        _comPort.doCommand(_comPort.MAV.sysid, _comPort.MAV.compid, MAVLink.MAV_CMD.DO_SET_SERVO,
+            n, pwm, 0, 0, 0, 0, 0));
+    Log($"Servo {n} -> {pwm} us");
+  }
+
+  [RelayCommand]
+  [Obsolete]
+  private async Task ToggleRelay(string number) {
+    if (!Connected) {
+      Messages += "Not connected.\n";
+      return;
+    }
+    if (!int.TryParse(number, out int idx)) {
+      return;
+    }
+    bool on = !_relayState[idx];
+    _relayState[idx] = on;
+    await Task.Run(() =>
+        _comPort.doCommand(_comPort.MAV.sysid, _comPort.MAV.compid, MAVLink.MAV_CMD.DO_SET_RELAY,
+            idx, on ? 1 : 0, 0, 0, 0, 0, 0));
+    Log($"Relay {idx} {(on ? "ON" : "OFF")}");
+  }
+
+  // ---- Payload Control tab ----
+  [ObservableProperty]
+  private double _gimbalPitch;
+
+  [ObservableProperty]
+  private double _gimbalYaw;
+
+  [RelayCommand]
+  [Obsolete]
+  private async Task PointGimbal() {
+    if (!Connected) {
+      Messages += "Not connected.\n";
+      return;
+    }
+    double pitch = GimbalPitch;
+    double yaw = GimbalYaw;
+    await Task.Run(() =>
+        _comPort.doCommand(_comPort.MAV.sysid, _comPort.MAV.compid, MAVLink.MAV_CMD.DO_MOUNT_CONTROL,
+            (float)pitch, 0, (float)yaw, 0, 0, 0, 2));
+    Log($"Gimbal pitch {pitch} yaw {yaw}");
+  }
+
+  [RelayCommand]
+  [Obsolete]
+  private async Task TriggerCamera() {
+    if (!Connected) {
+      Messages += "Not connected.\n";
+      return;
+    }
+    await Task.Run(() =>
+        _comPort.doCommand(_comPort.MAV.sysid, _comPort.MAV.compid, MAVLink.MAV_CMD.DO_DIGICAM_CONTROL,
+            0, 0, 0, 0, 1, 0, 0));
+    Log("Camera trigger sent");
+  }
+
+  private void Log(string m) => Messages += m + "\n";
 }
 
 public partial class ServoOut : ObservableObject {
